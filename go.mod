a1172bd0e365c5009cf3f158248239cda45977fb
cekys <10346089+cekys@users.noreply.github.com>
2020-09-21 19:16:41 +0800

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module startup
 
 go 1.15
 
-require github.com/cekys/gopkg v0.0.3
+require github.com/cekys/gopkg v0.0.5
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/cekys/gopkg"
 	"log"
 	"os"
 	"os/exec"
@@ -11,25 +12,25 @@ import (
 
 func main() {
 	// 读取配置文件
-	err := readJSON(&conf, DefaultConfig)
+	err := gopkg.ReadJSON(&conf, DefaultConfig)
 	if err != nil {
 		// 如果配置文件不存在则写入默认配置文件
 		if errors.Is(err, os.ErrNotExist) {
-			err = writeJSON(&defaultConf, DefaultConfig)
+			err = gopkg.WriteJSON(&defaultConf, DefaultConfig)
 			log.Fatal(err)
 		} else {
 			log.Fatal(err)
 		}
 	}
 	// 读取配置文件中每一项指令对象
-	for _, i := range conf {
+	for n, i := range conf {
 		if i.Enabled {
 			// 参数按空格分词
 			args := strings.Fields(i.Args)
 			// 组合指令
 			cmd := exec.Command(i.Command, args...)
 			// 打印组合后的指令
-			fmt.Println(cmd)
+			fmt.Printf("%d: %v\n", n+1, cmd)
 			// 运行指令,不等待指令运行结束
 			err = cmd.Start()
 			if err != nil {
