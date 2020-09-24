8ffeb050480648123be9df21c7d7e6a7cc31b608
cekys <10346089+cekys@users.noreply.github.com>
2020-09-24 13:45:51 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,18 +6,18 @@ cd ${BASEDIR}
 
 # AMD64
 # linux amd64
-env GOOS=linux GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-linux-amd64
+env GOOS=linux GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}_linux-amd64
 # macos amd64
-env GOOS=darwin GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-macos-amd64
+env GOOS=darwin GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}_macos-amd64
 # windows amd64
-env GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-win-x64.exe
+env GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}_win-x64.exe
 
 # ARM64
 # linux arm64
-#env GOOS=linux GOARCH=arm64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-linux-arm64
+#env GOOS=linux GOARCH=arm64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}_linux-arm64
 # macos arm64
-#env GOOS=darwin GOARCH=arm64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-macos-arm64
+#env GOOS=darwin GOARCH=arm64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}_macos-arm64
 
 # I386
 # windows i386
-env GOOS=windows GOARCH=386 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-win-i386.exe
+env GOOS=windows GOARCH=386 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}_win-i386.exe
