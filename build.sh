8e2a73358c21ab9da94e4079f7a8aacbe167cd94
cekys <10346089+cekys@users.noreply.github.com>
2020-10-28 14:08:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,12 +12,16 @@ env GOOS=darwin GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o build/Releas
 # windows amd64
 env GOOS=windows GOARCH=amd64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-win-amd64.exe
 
+# I386
+# windows i386
+env GOOS=windows GOARCH=386 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-win-i386.exe
+
 # ARM64
 # linux arm64
 env GOOS=linux GOARCH=arm64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-linux-arm64
 # macos arm64
 #env GOOS=darwin GOARCH=arm64 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-macos-arm64
 
-# I386
-# windows i386
-env GOOS=windows GOARCH=386 go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-win-i386.exe
+# ARM
+# linux arm
+env GOOS=linux GOARCH=arm go build -trimpath -ldflags "-s -w" -o build/Release/${PROGRAM}-linux-arm
